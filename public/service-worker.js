ff103583d795b909fdc1734c8a7218545dbb8c86
Pushkar Saraf <pushkars919@gmail.com>
2024-06-23 12:01:12 +0530

    index Routing fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,9 +8,16 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-	if (event.request.mode === 'navigate') {
-	  event.respondWith(
-		fetch(event.request).catch(() => caches.match('/index.html'))
-	  );
-	}
-});  
+	event.respondWith(
+	  caches.match(event.request).then((response) => {
+		return response || fetch(event.request).then((response) => {
+		  return caches.open(CACHE_NAME).then((cache) => {
+			cache.put(event.request, response.clone());
+			return response;
+		  });
+		});
+	  }).catch(() => {
+		return caches.match('/BirthdayReminder/index.html');
+	  })
+	);
+  });  
