c36e345d94a389aa3aad6963841a45e69ce63201
Pushkar Saraf <pushkars919@gmail.com>
2024-06-23 11:03:02 +0530

    PWA after installation routing

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,10 +8,9 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-	event.respondWith(
-		caches.match(event.request)
-			.then((response) => {
-				return response || fetch(event.request);
-			})
-	);
+	if (event.request.mode === 'navigate') {
+	  event.respondWith(
+		fetch(event.request).catch(() => caches.match('/index.html'))
+	  );
+	}
 });  
